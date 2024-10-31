2e2da0ac57485a297173c2a3aebedd4f721c1297
Jake Hillman <66786161+hillmanation@users.noreply.github.com>
2024-10-31 00:46:51 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -166,7 +166,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'night',
+    defaultTheme: 'dim',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
